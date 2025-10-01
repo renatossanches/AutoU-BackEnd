08d6289cfede7f3892b51029ffbe9c3c6ac70c55
renatossanches <renato.sanches2004@gmail.com>
2025-10-01 16:34:43 -0300

    Fix - bug

--- app/services/UserService.py
+++ app/services/UserService.py
@@ -1,14 +1,15 @@
 from sqlalchemy.orm import Session
 from app.repositories.UserRepository import get_user_by_email, create_user
 from app.utils.Security import hash_password, verify_password, create_access_token
-from app.dtos.response import UserResponse, LoginResponse
+from app.dtos.response.UserResponseDto import UserResponseDto
+from app.dtos.response.LoginResponseDto import LoginResponseDto
 def register_user(db: Session, email: str, password: str, name: str):
     if get_user_by_email(db, email):
         raise Exception("User already exists")
     hashed = hash_password(password)
     return create_user(db, email, hashed, name)
 
-def login_user(db: Session, email: str, password: str) -> UserResponse:
+def login_user(db: Session, email: str, password: str) -> UserResponseDto:
     user = get_user_by_email(db, email)
     if not user or not verify_password(password, user.password):
         raise Exception("Invalid credentials")
@@ -16,4 +17,4 @@ def login_user(db: Session, email: str, password: str) -> UserResponse:
     token = create_access_token({"sub": user.email})
     
     # Retorna dados do usuário + token
-    return LoginResponse(id=user.id, email=user.email, name=user.name, access_token=token)
+    return LoginResponseDto(id=user.id, email=user.email, name=user.name, access_token=token)
