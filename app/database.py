121314ee9a2cd955dfdad90430af21cdcd332c7f
renatossanches <renato.sanches2004@gmail.com>
2025-10-01 15:57:19 -0300

    Fix - retry database and webservice

--- app/database.py
+++ app/database.py
@@ -2,12 +2,19 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
 
 # ===== Variáveis de ambiente =====
 DATABASE_URL = os.environ.get("DATABASE_URL")  # Definida no Railway
 
 # ===== Configuração SQLAlchemy =====
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,      # verifica a conexão antes de usar
+    pool_size=10,            # quantidade de conexões no pool
+    max_overflow=20,         # conexões extras além do pool
+    pool_recycle=1800        # reinicia conexão após 30 min (em segundos)
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
@@ -20,6 +27,10 @@ def init_db():
 def get_db():
     db = SessionLocal()
     try:
+        yield db
+    except OperationalError:
+        db.close()
+        db = SessionLocal()
         yield db
     finally:
         db.close()
